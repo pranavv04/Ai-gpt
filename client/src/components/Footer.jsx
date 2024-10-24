3a63fe49021bbb42328c2d7c0603d725af21dd8a
Pranav Dhangar <113121206+pranavv04@users.noreply.github.com>
2024-10-24 19:39:03 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <h1 className='text-sm font-bold '>Created and Designed by Pranav</h1>
       <div className='flex flex-row '>
         <a href="https://github.com/pranavv04" target='__blank'><FaGithub className='mr-2 text-2xl' /></a>
-      <a href="www.linkedin.com/in/pranav-dhangar-26b67a220" target='__blank'>  <FaLinkedin  className='mr-2 text-2xl'/></a>
+      <a href="https://linkedin.com/in/pranav-dhangar-26b67a220" target='__blank'>  <FaLinkedin  className='mr-2 text-2xl'/></a>
       <a href=""><FaInstagram className='mr-2 text-2xl' /></a>
       <a href="https://x.com/_pranavv04_" target='__blank'>  <FaTwitterSquare  className='mr-2 text-2xl'/></a>
     
